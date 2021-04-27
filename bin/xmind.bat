e944d588097769e3f5a7c82da1b0bc0541b01839
陈泽功 <781015928@qq.com>
2021-04-27 09:18:06 +0800

    Init

--- bin/xmind.bat
+++ bin/xmind.bat
@@ -1,10 +1,9 @@
-@echo 输入XMind路径 %1
-@echo BLOG_HOME %BLOG_HOME%
+@echo JAVA_HOME %JAVA_HOME%
+@echo  %*
 
-java -Xbootclasspath/a:./ -jar ../libs/Xmind2MD-1.0.0.jar
+java   -jar libs/Xmind2MD-1.0.0.jar %*
 
 
-python %BLOG_HOME%\bin\xmind2md.py -source %1
 
 
 
